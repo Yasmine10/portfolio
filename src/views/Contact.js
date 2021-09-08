77ab8991558b22ad7edf51322e1dda79946d3891
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-09-08 23:03:30 +0200

    made contact component responsive

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -4,17 +4,21 @@ import Image from "../assets/images/female_reading_mail.png";
 class Contact extends Component {
   render() {
     return (
-      <div id="contact" className="contact-wrapper">
+      <div id="contact" className="border-pink">
         <div className="container py-5">
-          <div className="row">
-            <div className="image-wrapper col-6">
-              <img src={Image} alt="female reading mail" width="400" />
+          <div className="d-md-flex align-items-md-center">
+            <div className="flex-md-shrink-0 text-center text-md-start mb-5 mb-md-0">
+              <img
+                src={Image}
+                alt="female reading mail"
+                className="contact-image me-xl-5"
+              />
             </div>
-            <div className="contact-form-wrapper col-6">
-              <h4 className="page-title mb-4">
-                <span>Contact Me</span>
-              </h4>
+            <div className="flex-md-grow-1 ms-lg-5 ms-md-3 p-3 p-md-0">
               <form>
+                <h4 className="page-title mb-5 text-center text-md-start">
+                  <span>Contact Me</span>
+                </h4>
                 <div className="mb-3">
                   <input
                     type="text"
@@ -35,13 +39,15 @@ class Contact extends Component {
                   <textarea
                     className="form-control"
                     id="message"
-                    rows="4"
+                    rows="6"
                     placeholder="Message"
                   ></textarea>
                 </div>
-                <button className="btn" type="submit">
-                  Send
-                </button>
+                <div className="text-center text-md-start">
+                  <button className="btn" type="submit">
+                    Send
+                  </button>
+                </div>
               </form>
             </div>
           </div>

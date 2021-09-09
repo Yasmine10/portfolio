20a756fb378eb74de3a8f0540793717e04741dff
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-09-09 14:13:43 +0200

    made home component responsive

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-import image from "../assets/images/female_programmer.png";
+import Image from "../assets/images/female_programmer.png";
 
 class Home extends Component {
   render() {
     return (
-      <div id="home">
-        <div className="container d-flex justify-content-between align-items-center my-5">
-          <div className="personal-info mt-5">
-            <h4>Hi, I'm</h4>
+      <div id="home" className="py-5 px-3">
+        <div className="container d-md-flex d-block justify-content-md-between align-items-sm-center py-md-5 py-3">
+          <div className="flex-grow-1 text-center text-md-start">
+            <h4 className="fw-normal">Hi, I'm</h4>
             <h1 className="name mb-4">Yasmine De Wolf</h1>
-            <p>
+            <p className="fw-light">
               I'm a frontend developer with a passion for creating responsive
-              and <br />
-              ux friendly websites.
+              and ux friendly websites.
             </p>
             <a
               href="https://www.dropbox.com/s/6zl7o8a4er89m7w/cv_yasmine_de_wolf.pdf?dl=0"
@@ -23,8 +22,8 @@ class Home extends Component {
               Resume
             </a>
           </div>
-          <div className="image-wrapper mt-4 me-5 mt-5 pt-5 mb-4">
-            <img src={image} alt="female programmer" width="400" />
+          <div className="flex-shrink-0 d-none d-md-block">
+            <img className="home-image" src={Image} alt="female programmer" />
           </div>
         </div>
         <div className="clipping"></div>

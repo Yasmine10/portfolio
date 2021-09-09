4255fbc7aa616968939c1a42f54f9505c5e58ee9
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-09-09 13:06:50 +0200

    made skills component responsive

--- src/views/Skills.js
+++ src/views/Skills.js
@@ -13,56 +13,38 @@ import {
 class Skills extends Component {
   render() {
     return (
-      <div id="skills" className="">
-        <div className="container my-5 pb-5">
+      <div id="skills" className="pb-4">
+        <div className="container py-3">
           <h4 className="page-title text-center mb-5 pt-5">
             <span>Skills</span>
           </h4>
-          <div className="prog-lang-wrapper d-flex justify-content-center align-items-center">
-            <div className="prog-lang text-center px-4">
-              <FontAwesomeIcon
-                icon={faHtml5}
-                className="fa-3x"
-              ></FontAwesomeIcon>
+          <div className="d-flex flex-row flex-wrap justify-content-center align-items-center">
+            <div className="text-center px-4">
+              <FontAwesomeIcon icon={faHtml5} size="3x"></FontAwesomeIcon>
               <p className="mt-2">HTML</p>
             </div>
-            <div className="prog-lang text-center px-4">
-              <FontAwesomeIcon
-                icon={faCss3Alt}
-                className="fa-3x"
-              ></FontAwesomeIcon>
+            <div className="text-center px-4">
+              <FontAwesomeIcon icon={faCss3Alt} size="3x"></FontAwesomeIcon>
               <p className="mt-2">CSS</p>
             </div>
-            <div className="prog-lang text-center px-4">
-              <FontAwesomeIcon icon={faJs} className="fa-3x"></FontAwesomeIcon>
+            <div className="text-center px-4">
+              <FontAwesomeIcon icon={faJs} size="3x"></FontAwesomeIcon>
               <p className="mt-2">JavaScript</p>
             </div>
-            <div className="prog-lang text-center px-4">
-              <FontAwesomeIcon
-                icon={faBootstrap}
-                className="fa-3x"
-              ></FontAwesomeIcon>
+            <div className="text-center px-4">
+              <FontAwesomeIcon icon={faBootstrap} size="3x"></FontAwesomeIcon>
               <p className="mt-2">Bootstrap</p>
             </div>
-            <div className="prog-lang text-center px-4">
-              <FontAwesomeIcon
-                icon={faVuejs}
-                className="fa-3x"
-              ></FontAwesomeIcon>
+            <div className="text-center px-4">
+              <FontAwesomeIcon icon={faVuejs} size="3x"></FontAwesomeIcon>
               <p className="mt-2">VueJs</p>
             </div>
-            <div className="prog-lang text-center px-4">
-              <FontAwesomeIcon
-                icon={faAngular}
-                className="fa-3x"
-              ></FontAwesomeIcon>
+            <div className="text-center px-4">
+              <FontAwesomeIcon icon={faAngular} size="3x"></FontAwesomeIcon>
               <p className="mt-2">Angular CLI</p>
             </div>
-            <div className="prog-lang text-center px-4">
-              <FontAwesomeIcon
-                icon={faJava}
-                className="fa-3x"
-              ></FontAwesomeIcon>
+            <div className="text-center px-4">
+              <FontAwesomeIcon icon={faJava} size="3x"></FontAwesomeIcon>
               <p className="mt-2">Java</p>
             </div>
           </div>

7fe05519e1febfb498b8631c3f58e497f967dfc0
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-09-21 12:57:16 +0200

    added slider component and added it to project card component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,53 +2,74 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import Slider from "./Slider";
 
 function ProjectCard(props) {
   const languages = props.project.languages;
   const listItems = languages.map((language, index) => (
-    <li key={index} className="list-item-text me-4">
+    <li key={index} className="languages-list-item-text me-4">
       {language}
+    </li>
+  ));
+
+  const descriptionItems = props.project.description.map((alinea, index) => (
+    <li key={index} className="">
+      <span>{alinea}</span>
     </li>
   ));
 
   return (
     <div
       id="project-card"
-      className="d-md-flex align-items-md-center pt-5 mt-3 d-block"
+      className="d-md-flex align-items-md-center pt-5 mt-3 d-block row"
     >
-      <div className="flex-shrink-md-0 me-lg-5">
-        <img
+      <div
+        // className="flex-shrink-md-0 me-lg-5"
+        className="col-lg-6 col-12 me-lg-5"
+      >
+        {/* <img
           src={props.project.imageUrl}
           alt={props.project.imageAlt}
           className="project-image"
-        />
+        /> */}
+        <Slider images={props.project.images} />
       </div>
-      <div className="flex-grow-md-1">
+      <div
+        // className="flex-grow-md-1"
+        className="col-lg-5 col-12"
+      >
         <h5 className="project-title text-uppercase pt-3">
           <span>{props.project.name}</span>
         </h5>
-        <p className="fw-light">{props.project.description}</p>
-        <ul className="list d-flex flex-row flex-wrap">{listItems}</ul>
+        <ul className="description-list mb-4">
+          {descriptionItems}
+        </ul>
+        {/* <p className="fw-light">{props.project.description}</p> */}
+        <ul className="languages-list d-flex flex-row flex-wrap">{listItems}</ul>
         <div className="project-card-icons">
-          <a
-            href={props.project.liveLink}
-            className=" me-3"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faExternalLinkAlt}
-              size="lg"
-            ></FontAwesomeIcon>
-          </a>
-          <a
-            href={props.project.github}
-            className=""
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon icon={faGithub} size="lg"></FontAwesomeIcon>
-          </a>
+          {props.project.liveLink && (
+            <a
+              href={props.project.liveLink}
+              className=" me-3"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faExternalLinkAlt}
+                size="lg"
+              ></FontAwesomeIcon>
+            </a>
+          )}
+          {props.project.github && (
+            <a
+              href={props.project.github}
+              className=""
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} size="lg"></FontAwesomeIcon>
+            </a>
+          )}
         </div>
       </div>
     </div>
